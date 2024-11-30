be7a8f2e864afbdc6fd9317cf8a9885b06918d51
pakaremon <nguyenthanhcong012002@gmail.com>
2024-11-30 09:15:57 +0700

    first commit

---
+++ socket.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+
+int main() {
+    int sockfd;
+    struct sockaddr_in server_addr;
+    struct hostent *server;
+
+    // Create socket
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0) {
+        perror("Error opening socket");
+        exit(1);
+    }
+
+    // Get server by name
+    server = gethostbyname("google.com");
+    if (server == NULL) {
+        fprintf(stderr, "Error, no such host\n");
+        exit(1);
+    }
+
+    // Set up server address structure
+    memset(&server_addr, 0, sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    memcpy(&server_addr.sin_addr.s_addr, server->h_addr, server->h_length);
+    server_addr.sin_port = htons(80);
+
+    // Connect to server
+    if (connect(sockfd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
+        perror("Error connecting");
+        close(sockfd);
+        exit(1);
+    }
+
+    printf("Connected to google.com\n");
+
+    // Close socket
+    close(sockfd);
+    return 0;
+}
